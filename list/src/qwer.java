30ecc9aaeb295d35c38fbd57a1a457c5978fab8f
Kuistal9 <artemyludskay@gmail.com>
2023-02-04 10:31:02 +0300

    1

---
+++ list/src/qwer.java
@@ -0,0 +1,13 @@
+public class qwer {
+            public static void main(String[] args) {
+
+                int count;
+                for (count = 0; count < 5; count = count + 1){
+                    System.out.println("переменная цикла " + count);
+                }
+                    System.out.println("цикл окончен" );
+            }
+ }
+
+// демонстрация цикла Рог
+// сохраните исходный код в файл с именем
